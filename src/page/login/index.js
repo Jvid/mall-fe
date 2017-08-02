23277f6242e4dbdbd453ec4129a42f4af4c49909
jingduo <jingduo420@126.com>
2017-08-02 21:52:20 +0800

    webpackconfig

---
+++ src/page/login/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: jingduo
+* @Date:   2017-08-01 21:10:40
+* @Last Modified by:   jingduo
+* @Last Modified time: 2017-08-01 21:38:49
+*/
+
+'use strict';
+require('../module.js');
+console.log('hello login')
---
+++ src/page/module.js
@@ -0,0 +1,11 @@
+/*
+* @Author: jingduo
+* @Date:   2017-08-01 21:37:13
+* @Last Modified by:   jingduo
+* @Last Modified time: 2017-08-01 21:37:16
+*/
+
+'use strict';
+module.exports = {
+	test : 1234
+}
